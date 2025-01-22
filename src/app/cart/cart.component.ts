2a6fb169acdce9b3f6ec644d7a6219e006eaad71
anas2025-collab <sheikhanas2023@gmail.com>
2025-01-22 12:25:00 +0530

    pushing first time to phase_1

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,3 +1,5 @@
+/*
+
 // cart.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
@@ -20,5 +22,37 @@ export class CartComponent implements OnInit {
   clearCart() {
     this.productService.clearCart();
     this.cart = [];
+  }
+}
+
+*/
+
+
+
+// cart.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  cart = this.productService.getCart();
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.refreshCart();
+  }
+
+  refreshCart() {
+    this.cart = this.productService.getCart();
+  }
+
+  clearCart() {
+    this.productService.clearCart();
+    this.refreshCart(); // Refresh the cart view after clearing it
   }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,3 +1,5 @@
+/*
+
 // product-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
@@ -26,5 +28,45 @@ export class ProductListComponent implements OnInit {
   deleteProduct(id: number) {
     this.productService.deleteProduct(id);
     this.products = this.productService.getProducts(); // Refresh the list
+  }
+}
+
+*/
+
+
+
+// product-list.component.ts
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  newProduct: Product = { id: 0, name: '', description: '', price: 0 };
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.refreshProducts();
+  }
+
+  refreshProducts() {
+    this.products = this.productService.getProducts();
+  }
+
+  addProduct() {
+    this.productService.addProduct(this.newProduct);
+    this.newProduct = { id: 0, name: '', description: '', price: 0 }; // Reset form
+    this.refreshProducts(); // Refresh the product list
+  }
+
+  deleteProduct(id: number) {
+    this.productService.deleteProduct(id);
+    this.refreshProducts(); // Refresh the product list after deletion
   }
 }
